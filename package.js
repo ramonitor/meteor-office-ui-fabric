0fbbee2394ba5e02db14cb4bf261ce424875bdba
ramonitor <rhansman@Ramons-MacBook-Pro.local>
2016-01-18 22:05:54 +0100

    update to Office UI Fabric 1.2.1

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'ramonitor:office-ui-fabric',
-  version: '0.0.2',
-  summary: 'Office UI Fabric 1.1.3',
+  version: '0.0.3',
+  summary: 'Office UI Fabric 1.2.1',
   git: 'https://github.com/ramonitor/meteor-office-ui-fabric',
   documentation: 'README.md'
 });
